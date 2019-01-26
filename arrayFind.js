00c69fb3905052c652cac88b50514f7443b7d7fc
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2019-01-26 11:29:14 +0800

    Added arrayFind.js which demonstrates searching an array for an object matching a field to a value.

---
+++ arrayFind.js
@@ -0,0 +1,26 @@
+let testArray = [
+    {
+        id: 1,
+        mode: 'response',
+        sequence: 'solitary'
+    },
+    {
+        id: 2,
+        mode: 'test',
+        sequence: 'pre'
+    },
+    {
+        id: 3,
+        mode: 'test',
+        sequence: 'post'
+    }
+];
+
+if (process.argv.length < 5)
+{
+    console.log('Usage: ' + process.argv[1] + ' ' + process.argv[2] + ' <field> <value>');
+    process.exit(4);
+}
+
+let found = testArray.find((x) => x[process.argv[3]] === process.argv[4]);
+console.dir(found);
