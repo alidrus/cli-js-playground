a682032710ac7f7605e222a8d5453c8491943bab
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2019-02-17 21:52:23 +0800

    Testing instanceof vs typeof

---
+++ instanceof.js
@@ -0,0 +1,20 @@
+let questionNumber = 1,
+    choices = ['A', 'B', 'C', 'D', 'E'],
+    answer = 'A';
+
+console.log("typeof questionNumber === 'number'");
+console.log(typeof questionNumber === 'number');
+console.log("questionNumber instanceof Number");
+console.log(questionNumber instanceof Number);
+console.log('');
+
+console.log("typeof choices === 'array'");
+console.log(typeof choices === 'array');
+console.log("choices instanceof Array");
+console.log(choices instanceof Array);
+console.log('');
+
+console.log("typeof answer === 'string'");
+console.log(typeof answer === 'string');
+console.log("answer instanceof String");
+console.log(answer instanceof String);
