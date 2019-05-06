f5e8c31b1c6a114b98a87f1bf26b52228769c1e7
Abu Bakar Al-Idrus <alidrus@gmail.com>
2019-05-06 08:14:09 +0800

    Testing script for regular expression matching.

---
+++ regexMatch.js
@@ -0,0 +1,15 @@
+/**
+ * vim: syntax=javascript expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+ */
+
+let samples = [
+    '123',
+    '.',
+    '0.00',
+    '123456.00'
+];
+
+samples.forEach((sample) => {
+    let matches = sample.match(/^([1-9][0-9]*|0)(.[0-9][0-9])?$/);
+    console.dir(matches);
+});
