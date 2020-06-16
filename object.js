7767a6508046d50d18355c5f363acd88b825d5c4
Abu Bakar Al-Idrus <alidrus@gmail.com>
2020-06-16 20:49:06 +0800

    Added functions in subobject to test for "this"

--- object.js
+++ object.js
@@ -4,14 +4,29 @@
 
 let testObj = {
     num: 1,
-    getNum: () => {
-        return testObj.num;
+    getNum: function() {
+        return this.num;
     },
-    setNum: (newNum) => {
-        testObj.num = newNum;
+    setNum: function(newNum) {
+        this.num = newNum;
+    },
+    subObj: {
+        num: 99,
+        getNum: function() {
+            return this.num;
+        },
+        setNum: function(newNum) {
+            this.num = newNum;
+        }
     }
 };
 
 console.dir(testObj.getNum());
+console.dir(testObj.subObj.getNum());
+
 testObj.setNum(2);
+testObj.subObj.setNum(100);
+
+
 console.dir(testObj.getNum());
+console.dir(testObj.subObj.getNum());
