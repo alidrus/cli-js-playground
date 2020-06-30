9713b7b1cf1044f9ffb41a6dc9417fc3d8754daf
Abu Bakar Al-Idrus <alidrus@xdms.com>
2020-06-30 15:22:11 +0000

    Removed comment sample data. Removed console.log(). Use fs.writeFile() instead to write directly to php script.

--- countries.js
+++ countries.js
@@ -10,43 +10,33 @@ let countries = fs.readFileSync("./countries.json", "utf8");
 
 let data = JSON.parse(countries);
 
-/*
-{
-  alpha2: 'ZM',
-      alpha3: 'ZMB',
-      countryCallingCodes: [ '+260'  ],
-      currencies: [ 'ZMW'  ],
-      emoji: '🇿🇲',
-      ioc: 'ZAM',
-      languages: [ 'eng'  ],
-      name: 'Zambia',
-      status: 'assigned'
-    
-} 
- */
-
-let sanitize = (text) => {
+let sanitize = (text, quote = true) => {
     if (text)
     {
         const newText = text.replace(/\'/g, "\\\'");
-        return "'" + newText + "'";
+        return (quote ? "'" : "") + newText + (quote ? "'" : "");
     }
 
     return "null";
 };
 
-console.log("<?php");
+let contents = "<?php\n\n";
+
 data.forEach(country => {
     if (country.status !== "assigned" || country.currencies.length === 0 || country.countryCallingCodes.length === 0)
     {
         return;
     }
-    let create = "Country::create([\n"
+    contents += "// " + sanitize(country.name, false) + "\n"
+        + "Country::create([\n"
         + "    'name'            => " + sanitize(country.name) + ",\n"
         + "    'calling_code'    => " + sanitize(country.countryCallingCodes[0]) + ",\n"
         + "    'iso_3166_alpha2' => " + sanitize(country.alpha2) + ",\n"
         + "    'emoji_flag'      => " + sanitize(country.emoji) + ",\n"
         + "    'currency'        => " + sanitize(country.currencies[0]) + ",\n"
-        + "]);\n"
-    console.log(create);
+        + "]);\n\n";
+});
+
+fs.writeFile("countries.php", contents, () => {
+    console.log("written to countries.php");
 });
