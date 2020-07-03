94f54fd8e43d050ff781662b549e95d0830bb279
Abu Bakar Al-Idrus <alidrus@gmail.com>
2020-07-03 19:24:03 +0800

    Group the declaration and console.log() lines together for each test.

--- instanceof.js
+++ instanceof.js
@@ -4,29 +4,30 @@
  * vim: syntax=javascript expandtab tabstop=4 shiftwidth=4 softtabstop=4:
  */
 
-let questionNumber = 1,
-    choices = ['A', 'B', 'C', 'D', 'E'],
-    answer = 'A',
-    someObj = {a: 'a', b: 'b'};
-
+let questionNumber = 1;
+console.log("questionNumber = 1");
 console.log("typeof questionNumber === 'number'");
 console.log(typeof questionNumber === 'number');
 console.log("questionNumber instanceof Number");
 console.log(questionNumber instanceof Number);
 console.log('');
 
-console.log("typeof choices === 'array'");
-console.log(typeof choices === 'array');
+let choices = ['A', 'B', 'C', 'D', 'E'];
+console.log("choices = ['A', 'B', 'C', 'D', 'E']");
+console.log("typeof choices === " + (typeof choices) + "\ntrue");
 console.log("choices instanceof Array");
 console.log(choices instanceof Array);
 console.log('');
 
-console.log("typeof answer === 'string'");
-console.log(typeof answer === 'string');
+let answer = 'A';
+console.log("answer = 'A'");
+console.log("typeof answer === " + (typeof answer) + "\ntrue");
 console.log("answer instanceof String");
 console.log(answer instanceof String);
+console.log('');
 
-console.log("typeof someObj === 'object'");
-console.log(typeof someObj === 'object');
+let someObj = {a: 'a', b: 'b'};
+console.log("someObj = {a: 'a', b: 'b'}");
+console.log("typeof someObj === " + (typeof someObj) + "\ntrue");
 console.log("someObj instanceof Object");
 console.log(someObj instanceof Object);
