8d2d6397bb1a53aab65d7c2838f33bf18a65f0d9
Abu Bakar Al-Idrus <alidrus@xdms.com>
2020-07-05 05:00:50 +0000

    Added script to test array splicing.

---
+++ splice.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S node --harmony
+
+/**
+ * vim: syntax=javascript expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+ */
+
+// Original array with only 5 elements
+const argv = ["first", "second", "third", "fourth", "fifth"];
+
+// Remove 5 elements
+const argvCopy = Array.from(argv);
+argvCopy.splice(0, 5);
+console.dir(argvCopy);
+
+// Remove 6 elements
+const argvCopy2 = Array.from(argv);
+argvCopy2.splice(0, 6);
+console.dir(argvCopy2);
