2ae0644e0f87846637ea633d90288aba6ce102ee
Abu Bakar Al-Idrus <alidrus@gmail.com>
2020-09-06 10:34:05 +0800

    Use string formatting in console.log

--- instanceof.js
+++ instanceof.js
@@ -5,29 +5,24 @@
  */
 
 let questionNumber = 1;
-console.log("questionNumber = 1");
-console.log("typeof questionNumber === 'number'");
-console.log(typeof questionNumber === 'number');
-console.log("questionNumber instanceof Number");
-console.log(questionNumber instanceof Number);
+console.log("questionNumber = %o", questionNumber);
+console.log("(typeof questionNumber === 'number') evaluates to %o", typeof questionNumber === 'number');
+console.log("(questionNumber instanceof Number) evualuates to %o", questionNumber instanceof Number);
 console.log('');
 
 let choices = ['A', 'B', 'C', 'D', 'E'];
-console.log("choices = ['A', 'B', 'C', 'D', 'E']");
-console.log("typeof choices === " + (typeof choices) + "\ntrue");
-console.log("choices instanceof Array");
-console.log(choices instanceof Array);
+console.log("choices = %o", choices);
+console.log("(typeof choices === 'object') evaluates to %o", typeof choices === 'object');
+console.log("(choices instanceof Array) evaluates to %o", choices instanceof Array);
 console.log('');
 
 let answer = 'A';
-console.log("answer = 'A'");
-console.log("typeof answer === " + (typeof answer) + "\ntrue");
-console.log("answer instanceof String");
-console.log(answer instanceof String);
+console.log("answer = %o", answer);
+console.log("(typeof answer === 'string') evaluates to %o", (typeof answer === 'string'));
+console.log("(answer instanceof String) evaluates to %o", answer instanceof String);
 console.log('');
 
 let someObj = {a: 'a', b: 'b'};
-console.log("someObj = {a: 'a', b: 'b'}");
-console.log("typeof someObj === " + (typeof someObj) + "\ntrue");
-console.log("someObj instanceof Object");
-console.log(someObj instanceof Object);
+console.log("someObj = %o", someObj);
+console.log("(typeof someObj === 'object') evaluates to %o", (typeof someObj === 'object'));
+console.log("(someObj instanceof Object) evaluates to %o", someObj instanceof Object);
