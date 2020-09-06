bed44f2dcc7fac9e96faa6f892c69866a891f856
Abu Bakar Al-Idrus <alidrus@gmail.com>
2020-09-06 10:35:33 +0800

    Testing parseInt() in various scenarios.

---
+++ parseInt.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S node --harmony
+
+/**
+ * vim: syntax=javascript expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+ */
+
+const numberString = "22";
+const numberInt = 22;
+const nonNumber = "some random string";
+const mixedAlpha1 = "10littleIndians";
+const mixedAlpha2 = "little10Indians";
+const mixedAlpha3 = "littleIndians10";
+
+console.log('%o parses to %s', numberString, parseInt(numberString, 10));
+console.log('%o parses to %s', numberInt, parseInt(numberInt, 10));
+console.log('%o parses to %s', nonNumber, parseInt(nonNumber, 10));
+console.log('%o parses to %s', mixedAlpha1, parseInt(mixedAlpha1, 10));
+console.log('%o parses to %s', mixedAlpha2, parseInt(mixedAlpha2, 10));
+console.log('%o parses to %s', mixedAlpha3, parseInt(mixedAlpha3, 10));
